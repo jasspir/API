7c5638be38bf1aa3f2aa6f8218cfc545a980bcd6
Jass <145933956+jasspir@users.noreply.github.com>
2024-07-18 18:39:27 -0400

    Create SneakEvent.java

---
+++ src/main/java/me/jass/api/SneakEvent.java
@@ -0,0 +1,36 @@
+package me.jass.api;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class SneakEvent extends Event implements Cancellable {
+    private HandlerList handlers;
+    private boolean cancelled;
+    private boolean sneaking;
+
+    public SneakEvent(boolean sneaking) {
+        this.sneaking = sneaking;
+        handlers = new HandlerList();
+        cancelled = false;
+    }
+
+    public boolean isSneaking() {
+        return sneaking;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+}
